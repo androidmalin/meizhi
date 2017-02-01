a854f0f247cad834d0f626028f9823feba4623c2
androidmalin <malin.myemail@163.com>
2017-02-02 01:12:43 +0800

    add the image loading view

--- app/src/main/java/meizhi/meizhi/malin/adapter/ImagePagerAdapter.java
+++ app/src/main/java/meizhi/meizhi/malin/adapter/ImagePagerAdapter.java
@@ -5,10 +5,14 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 
@@ -20,6 +24,7 @@ import uk.co.senab.photoview.PhotoView;
 public class ImagePagerAdapter extends PagerAdapter {
 
 
+    private static final String TAG = ImagePagerAdapter.class.getSimpleName();
     private ArrayList<ImageBean> mList;
     private Context mContext;
     private int mItemWidth;
@@ -62,25 +67,50 @@ public class ImagePagerAdapter extends PagerAdapter {
         downLoad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mDownLoadClickListener == null) return;
                 mDownLoadClickListener.downImageListener(mList.get(position).url);
             }
         });
 
-        photoView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                mDownLoadClickListener.downImageListener(mList.get(position).url);
-                return false;
-            }
-        });
+        final ImageView holder = (ImageView) rootView.findViewById(R.id.iv_holder);
+
+        Glide.with(mContext)
+                .load(R.drawable.image_loading_holder_two)
+                .asGif()
+                .override((int) PhoneScreenUtil.dipToPx(mContext, 100.0f), (int) PhoneScreenUtil.dipToPx(mContext, 100.0f))
+                .into(holder);
 
         Glide.with(mContext)
                 .load(mList.get(position).url + "?imageView2/0/w/500")
-                .asBitmap()
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    //这个用于监听图片是否加载完成
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        holder.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .centerCrop()
                 .override(mItemWidth, mItemHeight)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(photoView);
+
+
+        photoView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (mDownLoadClickListener != null) {
+                    mDownLoadClickListener.downImageListener(mList.get(position).url);
+                }
+                return false;
+            }
+        });
+
 
         // Now just add PhotoView to ViewPager and return it
         container.addView(rootView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
