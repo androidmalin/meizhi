7cbc9b28b11a44e61b5c18d5e51d004814b79fc6
androidmalin <malin.myemail@163.com>
2017-02-03 23:56:20 +0800

    add the umeng activity config

--- app/src/main/java/meizhi/meizhi/malin/activity/AboutActivity.java
+++ app/src/main/java/meizhi/meizhi/malin/activity/AboutActivity.java
@@ -141,4 +141,16 @@ public class AboutActivity extends AppCompatActivity implements View.OnClickList
             Toast.makeText(this, R.string.no_browser_tip, Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
 }
