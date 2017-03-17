c451f422628d76d0d1d73096b5879b772c5f9004
androidmalin <malin.myemail@163.com>
2017-03-17 14:33:40 +0800

    add fresco load data/data/files/image.png

--- app/src/main/java/meizhi/meizhi/malin/activity/FrescoLoadUtil.java
+++ app/src/main/java/meizhi/meizhi/malin/activity/FrescoLoadUtil.java
@@ -1,20 +1,21 @@
 package meizhi.meizhi.malin.activity;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import com.facebook.common.executors.CallerThreadExecutor;
 import com.facebook.common.executors.UiThreadImmediateExecutorService;
 import com.facebook.common.references.CloseableReference;
+import com.facebook.common.util.UriUtil;
 import com.facebook.datasource.DataSource;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.common.ResizeOptions;
-import com.facebook.imagepipeline.core.ImagePipeline;
 import com.facebook.imagepipeline.core.ImagePipelineFactory;
 import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
 import com.facebook.imagepipeline.image.CloseableImage;
@@ -47,7 +48,99 @@ public final class FrescoLoadUtil {
         return instance;
     }
 
+    /**
+     * 加载本地应用中file文件夹中的图片
+     * @param simpleDraweeView SimpleDraweeView
+     * @param width width
+     * @param height height
+     * @param drawable drawable
+     */
+    public void LoadImageLocalFile(SimpleDraweeView simpleDraweeView,int width, int height, String drawable) {
+        Uri uri = new Uri.Builder()
+                .scheme(UriUtil.LOCAL_FILE_SCHEME)
+                .path(String.valueOf(drawable))
+                .build();
+        ImageRequest imageRequest = ImageRequestBuilder
+                .newBuilderWithSource(uri)
+                .setProgressiveRenderingEnabled(true)
+                .setResizeOptions(new ResizeOptions(width, height))
+                .build();
+        PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+                .setImageRequest(imageRequest)
+                .setOldController(simpleDraweeView.getController())
+                .setTapToRetryEnabled(false)
+                .setAutoPlayAnimations(true)
+                .build();
+        simpleDraweeView.setController(controller);
+    }
+
+
+    /**
+     * 加载Res文件夹中Drawable中的图片
+     * @param simpleDraweeView SimpleDraweeView
+     * @param width width
+     * @param height height
+     * @param drawable drawable
+     */
+    public void LoadImageLocalRes(SimpleDraweeView simpleDraweeView,int width, int height, @DrawableRes int drawable) {
+        Uri uri = new Uri.Builder()
+                .scheme(UriUtil.LOCAL_RESOURCE_SCHEME)
+                .path(String.valueOf(drawable))
+                .build();
+        ImageRequest imageRequest = ImageRequestBuilder
+                .newBuilderWithSource(uri)
+                .setProgressiveRenderingEnabled(true)
+                .setResizeOptions(new ResizeOptions(width, height))
+                .build();
+        PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+                .setImageRequest(imageRequest)
+                .setOldController(simpleDraweeView.getController())
+                .setTapToRetryEnabled(false)
+                .setAutoPlayAnimations(true)
+                .build();
+        simpleDraweeView.setController(controller);
+    }
+
+
+    /**
+     * 加载一张网络图片
+     * @param simpleDraweeView　SimpleDraweeView
+     * @param width　width
+     * @param height height
+     * @param url http url
+     */
+    public void loadImageNetWork(SimpleDraweeView simpleDraweeView,int width ,int height, String url) {
+        ImageRequest imageRequest = ImageRequestBuilder
+                .newBuilderWithSource(Uri.parse(url))
+                //这里设置渐进式jpeg开关，记得在fresco初始化时设置progressiveJpegConfig
+                .setProgressiveRenderingEnabled(true)
+                //在解码之前修改图片尺寸
+                //缩放,在解码前修改内存中的图片大小, 配合Downsampling可以处理所有图片,否则只能处理jpg,
+                // 开启Downsampling:在初始化时设置.setDownsampleEnabled(true)
+                .setResizeOptions(new ResizeOptions(width, height))
+                .build();
+
+        PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+                .setImageRequest(imageRequest)
+                //在构建新的控制器时需要setOldController，这可以防止重新分配内存
+                .setOldController(simpleDraweeView.getController())
+                //tap-to-retry load image
+                .setTapToRetryEnabled(false)
+                //是否自动开启gif,webp动画,也可以在ControllerListener下手动启动动画
+                .setAutoPlayAnimations(true)
+                .build();
+        simpleDraweeView.setController(controller);
+    }
+
     //加载直接返回Bitmap
+
+    /**
+     * 加载一张网络图片，返回Bitmap
+     * @param url　http url
+     * @param width　ｗ
+     * @param height ｈ
+     * @param callback FrescoBitmapCallback<Bitmap>
+     */
     public final void loadImageBitmap(String url, final int width, final int height, FrescoBitmapCallback<Bitmap> callback) {
         if (TextUtils.isEmpty(url)) {
             return;
--- app/src/main/java/meizhi/meizhi/malin/activity/TActivity.java
+++ app/src/main/java/meizhi/meizhi/malin/activity/TActivity.java
@@ -1,20 +1,23 @@
 package meizhi.meizhi.malin.activity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewTreeObserver;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.common.ResizeOptions;
-import com.facebook.imagepipeline.request.ImageRequest;
-import com.facebook.imagepipeline.request.ImageRequestBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import meizhi.meizhi.malin.R;
 
@@ -24,19 +27,137 @@ import meizhi.meizhi.malin.R;
 
 public class TActivity extends AppCompatActivity {
 
-    private SimpleDraweeView simpleDraweeView;
+    private SimpleDraweeView mSVRoundView;
+    private SimpleDraweeView mSVLocalView;
+    private SimpleDraweeView mSVLocalFileView;
     private ImageView mImageView;
     private static final String IMAGE_URL = "http://ww2.sinaimg.cn/large/7a8aed7bgw1es8c7ucr0rj20hs0qowhl.jpg";
     private int mViewWidth;
     private int mViewHeight;
+    private String TAG = TActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tlayout);
-        simpleDraweeView = (SimpleDraweeView) findViewById(R.id.image_tt);
-        mImageView = (ImageView) findViewById(R.id.image_image);
+        initView();
+        loadSVRoundView();
+        getBitmap();
+        loadLocalData();
+        testWH();
 
+        String dir = getSignSubDir(this, "md5", "level_avatar", true);
+
+        File file = new File(dir, "key1");
+        if (!file.exists()) {
+            try {
+                boolean newFile = file.createNewFile();
+                if (!newFile) return;
+            } catch (IOException e) {
+                return;
+            }
+        }
+
+        Log.d(TAG, dir);
+        Log.d(TAG, file.getAbsolutePath());
+
+        initFileImage();
+    }
+
+
+    private void initFileImage() {
+        
+        String path = null;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pay_log);
+        if (bitmap == null) {
+            return;
+        }
+        String dir = getSignSubDir(this, "md5", "level_avatar", true);
+        File file = new File(dir, "key");
+        if (!file.exists()) {
+            try {
+                boolean newFile = file.createNewFile();
+                if (!newFile) return;
+            } catch (IOException e) {
+                Log.e(TAG, "createNewFile failed, file:" + "key" + " url:" + "url");
+                return;
+            }
+        }
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "FileNotFoundException, file:" + "key" + " url:" + "url");
+        }
+        if (null == out) return;
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+        try {
+            out.flush();
+            Log.d(TAG, file.getPath());
+            path =file.getPath();
+        } catch (IOException e) {
+            Log.e(TAG, "IOException flush, file:" + "key" + " url:" + "url");
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException close, file:" + "key" + " url:" + "url");
+            }
+        }
+
+
+        Log.d(TAG,"LoadImageLocalFile:"+path);
+        FrescoLoadUtil.getInstance().LoadImageLocalFile(mSVLocalFileView,100, 100,path);
+    }
+
+    private static String getSignSubDir(Context context, String sign, String subDir, boolean bCreate) {
+        String retDir = "";
+        String signDir = getSignDir(context, sign, bCreate);
+        if (TextUtils.isEmpty(signDir)) {
+            return retDir;
+        }
+        String dir = signDir + File.separator + subDir;
+        File fileDir = new File(dir);
+        if (fileDir.exists()) {
+            retDir = fileDir.getPath();
+        } else {
+            if (bCreate) {
+                if (fileDir.mkdirs()) {
+                    retDir = fileDir.getPath();
+                }
+            }
+        }
+        return retDir;
+    }
+
+
+    private static String getSignDir(Context context, String sign, boolean bCreate) {
+        String ret = "";
+        File filesDir = context.getFilesDir();
+        String dir = filesDir.getPath() + File.separator + sign;
+        File signDir = new File(dir);
+        if (signDir.exists()) {
+            ret = signDir.getPath();
+        } else {
+            if (bCreate) {
+                if (signDir.mkdirs()) {
+                    ret = signDir.getPath();
+                }
+            }
+        }
+        return ret;
+    }
+
+
+    private void loadLocalData() {
+        FrescoLoadUtil.getInstance().LoadImageLocalRes(mSVLocalView, 100, 100, R.drawable.square_app_icon);
+    }
+
+    private void loadSVRoundView() {
+        FrescoLoadUtil.getInstance().loadImageNetWork(mSVRoundView, 100, 100, IMAGE_URL);
+    }
+
+    private void getBitmap() {
         FrescoLoadUtil.getInstance().loadImageBitmap(IMAGE_URL, 100, 100, new FrescoBitmapCallback<Bitmap>() {
             @Override
             public void onSuccess(Uri uri, Bitmap result) {
@@ -54,60 +175,20 @@ public class TActivity extends AppCompatActivity {
                 Toast.makeText(TActivity.this, "onCancel", Toast.LENGTH_SHORT).show();
             }
         });
-
-//        int ww = simpleDraweeView.getMeasuredWidth();
-//        int hh = simpleDraweeView.getMeasuredHeight();
-//
-//        Toast.makeText(this, "w:"+ww+" "+"h:"+hh, Toast.LENGTH_SHORT).show();
-
-
-        mViewWidth = simpleDraweeView.getLayoutParams().width;
-        mViewHeight = simpleDraweeView.getLayoutParams().height;
-//        simpleDraweeView.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                Toast.makeText(TActivity.this, "w:"+simpleDraweeView.getWidth()+" "+"h:"+simpleDraweeView.getHeight(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-//        ViewTreeObserver vto = simpleDraweeView.getViewTreeObserver();
-//        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-//            public boolean onPreDraw() {
-//                mViewWidth = simpleDraweeView.getMeasuredWidth();
-//                mViewHeight = simpleDraweeView.getMeasuredHeight();
-//                return true;
-//            }
-//        });
-        Toast.makeText(this, "w:"+mViewWidth+" "+"h:"+mViewHeight, Toast.LENGTH_SHORT).show();
-        loadImgCode(simpleDraweeView,IMAGE_URL,100,100);
     }
 
 
+    private void initView() {
+        mSVRoundView = (SimpleDraweeView) findViewById(R.id.image_round);
+        mSVLocalView = (SimpleDraweeView) findViewById(R.id.sv_local_drawable);
+        mImageView = (ImageView) findViewById(R.id.image_image);
+        mSVLocalFileView = (SimpleDraweeView) findViewById(R.id.sv_local_file_img);
+    }
 
-
-    private void loadImgCode(SimpleDraweeView simpleDraweeView, String url, int w, int h) {
-        mViewWidth = simpleDraweeView.getLayoutParams().width;
-        mViewHeight = simpleDraweeView.getLayoutParams().height;
-        ImageRequest imageRequest = ImageRequestBuilder
-                .newBuilderWithSource(Uri.parse(url))
-                //这里设置渐进式jpeg开关，记得在fresco初始化时设置progressiveJpegConfig
-                .setProgressiveRenderingEnabled(true)
-                //在解码之前修改图片尺寸
-                //缩放,在解码前修改内存中的图片大小, 配合Downsampling可以处理所有图片,否则只能处理jpg,
-                // 开启Downsampling:在初始化时设置.setDownsampleEnabled(true)
-                .setResizeOptions(new ResizeOptions(w, h))
-                .build();
-
-        PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
-                .setImageRequest(imageRequest)
-                //在构建新的控制器时需要setOldController，这可以防止重新分配内存
-                .setOldController(simpleDraweeView.getController())
-                //tap-to-retry load image
-                .setTapToRetryEnabled(false)
-                //是否自动开启gif,webp动画,也可以在ControllerListener下手动启动动画
-                .setAutoPlayAnimations(true)
-                .build();
-        simpleDraweeView.setController(controller);
+    private void testWH() {
+        mViewWidth = mSVRoundView.getLayoutParams().width;
+        mViewHeight = mSVRoundView.getLayoutParams().height;
+        Toast.makeText(this, "w:" + mViewWidth + " " + "h:" + mViewHeight, Toast.LENGTH_SHORT).show();
     }
 
 }
