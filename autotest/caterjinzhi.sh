5d5383b4f2ee9e53944648ed9f4d59d6bff66837
androidmalin <malin.myemail@163.com>
2017-07-21 15:20:39 +0800

    update shell

--- autotest/caterjinzhi.sh
+++ autotest/caterjinzhi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-hexdump hexdump -C -n 512 pm.img
+hexdump -C -n 512 pm.img
+
+#hexdump -C -n 512 cover.jpg | awk '{for(i=NF; i>17; --i) print $i}'
 
 ##1.od -x pm.img # -x：用十六进制显示
 ##2.hexdump -C -n 512 pm.img # -n 512 ；只显示前512个字节的数据 -C：左边显示16进制，右边显示字符
