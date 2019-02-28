02bd5bff11784ac5f0889285eaeb733ce22e5241
androidmalin <malin.myemail@163.com>
2019-02-28 10:16:08 +0800

    change getapkinfo.sh and runApp.sh

--- autotest/huaweip9/getapkinfo.sh
+++ autotest/huaweip9/getapkinfo.sh
@@ -1,6 +1,1 @@
-#!/bin/bash 
-package=`aapt dump badging $* | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
-activity=`aapt dump badging $* | grep launchable-activity: | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
-#echo
-echo package : $package
-echo activity: $activity
+aapt dump badging applicaiton-debug.apk applicaiton-debug.apk |  grep versionName
